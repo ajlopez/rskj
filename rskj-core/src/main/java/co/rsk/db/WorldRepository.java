8de4e118246159b0e698ad2f2b348025ba7d5907
ajlopez <webmaster@ajlopez.com>
2018-10-13 17:18:22 -0300

    WorldRepository get code from account

--- rskj-core/src/main/java/co/rsk/db/WorldRepository.java
+++ rskj-core/src/main/java/co/rsk/db/WorldRepository.java
@@ -1,16 +1,19 @@
 package co.rsk.db;
 
-import co.rsk.core.Rsk;
 import co.rsk.core.RskAddress;
 import co.rsk.trie.Trie;
 import org.ethereum.core.AccountState;
+import org.ethereum.crypto.HashUtil;
 
-import static org.ethereum.util.ByteUtil.EMPTY_BYTE_ARRAY;
+import java.util.Arrays;
 
 /**
  * Created by ajlopez on 13/10/2018.
  */
 public class WorldRepository {
+    private static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
+    private static final byte[] EMPTY_DATA_HASH = HashUtil.keccak256(EMPTY_BYTE_ARRAY);
+
     private Trie trie;
 
     public WorldRepository(Trie trie) {
@@ -28,6 +31,24 @@ public class WorldRepository {
     }
 
     public byte[] getCode(RskAddress address) {
-        return EMPTY_BYTE_ARRAY;
+        AccountState accountState = this.getAccountState(address);
+
+        if (accountState == null || accountState.isHibernated()) {
+            return EMPTY_BYTE_ARRAY;
+        }
+
+        byte[] codeHash = accountState.getCodeHash();
+
+        if (Arrays.equals(codeHash, EMPTY_DATA_HASH)) {
+            return EMPTY_BYTE_ARRAY;
+        }
+
+        byte[] code = this.trie.get(codeHash);
+
+        if (code == null) {
+            return EMPTY_BYTE_ARRAY;
+        }
+
+        return code;
     }
 }
--- rskj-core/src/test/java/co/rsk/db/WorldRepositoryTest.java
+++ rskj-core/src/test/java/co/rsk/db/WorldRepositoryTest.java
@@ -7,6 +7,7 @@ import co.rsk.trie.TrieImpl;
 import co.rsk.trie.TrieStore;
 import co.rsk.trie.TrieStoreImpl;
 import org.ethereum.core.AccountState;
+import org.ethereum.crypto.HashUtil;
 import org.ethereum.datasource.HashMapDB;
 import org.junit.Assert;
 import org.junit.Test;
@@ -52,5 +53,60 @@ public class WorldRepositoryTest {
         Assert.assertNotNull(result);
         Assert.assertEquals(accountState.getBalance(), result.getBalance());
         Assert.assertEquals(accountState.getNonce(), result.getNonce());
+    }
+
+    @Test
+    public void getNewAccountCodeAsEmptyByteArray() {
+        RskAddress accountAddress = new RskAddress("0000000000000000000000000000000000001234");
+        AccountState accountState = new AccountState(BigInteger.ONE, Coin.ZERO);
+        Trie trie = new TrieImpl();
+        trie = trie.put(accountAddress.getBytes(), accountState.getEncoded());
+        WorldRepository repository = new WorldRepository(trie);
+
+        byte[] code = repository.getCode(new RskAddress("0000000000000000000000000000000000001234"));
+
+        Assert.assertNotNull(code);
+        Assert.assertEquals(0, code.length);
+    }
+
+    @Test
+    public void getAccountCode() {
+        byte[] code = new byte[] { 0x01, 0x02, 0x03 };
+        byte[] codeHash = HashUtil.keccak256(code);
+        RskAddress accountAddress = new RskAddress("0000000000000000000000000000000000001234");
+        AccountState accountState = new AccountState(BigInteger.ONE, Coin.ZERO);
+        accountState.setCodeHash(codeHash);
+
+        Trie trie = new TrieImpl();
+        trie = trie.put(accountAddress.getBytes(), accountState.getEncoded());
+        trie = trie.put(codeHash, code);
+
+        WorldRepository repository = new WorldRepository(trie);
+
+        byte[] result = repository.getCode(new RskAddress("0000000000000000000000000000000000001234"));
+
+        Assert.assertNotNull(result);
+        Assert.assertArrayEquals(code, result);
+    }
+
+    @Test
+    public void getHibernatedAccountCode() {
+        byte[] code = new byte[] { 0x01, 0x02, 0x03 };
+        byte[] codeHash = HashUtil.keccak256(code);
+        RskAddress accountAddress = new RskAddress("0000000000000000000000000000000000001234");
+        AccountState accountState = new AccountState(BigInteger.ONE, Coin.ZERO);
+        accountState.setCodeHash(codeHash);
+        accountState.hibernate();
+
+        Trie trie = new TrieImpl();
+        trie = trie.put(accountAddress.getBytes(), accountState.getEncoded());
+        trie = trie.put(codeHash, code);
+
+        WorldRepository repository = new WorldRepository(trie);
+
+        byte[] result = repository.getCode(new RskAddress("0000000000000000000000000000000000001234"));
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals(0, result.length);
     }
 }
