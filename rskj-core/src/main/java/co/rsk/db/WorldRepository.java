a32499fbff0a29aa25f5dc8e37be5fd90622c328
ajlopez <webmaster@ajlopez.com>
2018-10-13 16:48:30 -0300

    WorldRepository get account state

--- rskj-core/src/main/java/co/rsk/db/WorldRepository.java
+++ rskj-core/src/main/java/co/rsk/db/WorldRepository.java
@@ -1,13 +1,26 @@
 package co.rsk.db;
 
 import co.rsk.core.RskAddress;
+import co.rsk.trie.Trie;
 import org.ethereum.core.AccountState;
 
 /**
  * Created by ajlopez on 13/10/2018.
  */
 public class WorldRepository {
+    private Trie trie;
+
+    public WorldRepository(Trie trie) {
+        this.trie = trie;
+    }
+
     public AccountState getAccountState(RskAddress address) {
-        return null;
+        byte[] accountData = this.trie.get(address.getBytes());
+
+        if (accountData == null) {
+            return null;
+        }
+
+        return new AccountState(accountData);
     }
 }
--- rskj-core/src/test/java/co/rsk/db/WorldRepositoryTest.java
+++ rskj-core/src/test/java/co/rsk/db/WorldRepositoryTest.java
@@ -1,20 +1,45 @@
 package co.rsk.db;
 
+import co.rsk.core.Coin;
 import co.rsk.core.RskAddress;
+import co.rsk.trie.Trie;
+import co.rsk.trie.TrieImpl;
+import co.rsk.trie.TrieStore;
+import co.rsk.trie.TrieStoreImpl;
 import org.ethereum.core.AccountState;
+import org.ethereum.datasource.HashMapDB;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.math.BigInteger;
 
 /**
  * Created by ajlopez on 13/10/2018.
  */
 public class WorldRepositoryTest {
     @Test
-    public void getUnknownAccountState() {
-        WorldRepository repository = new WorldRepository();
+    public void getUnknownAccountStateAsNull() {
+        Trie trie = new TrieImpl();
+        WorldRepository repository = new WorldRepository(trie);
 
         AccountState accountState = repository.getAccountState(new RskAddress("0000000000000000000000000000000000001234"));
 
         Assert.assertNull(accountState);
+    }
+
+    @Test
+    public void getAccountState() {
+        RskAddress accountAddress = new RskAddress("0000000000000000000000000000000000001234");
+        AccountState accountState = new AccountState(BigInteger.ONE, Coin.ZERO);
+        Trie trie = new TrieImpl();
+        trie = trie.put(accountAddress.getBytes(), accountState.getEncoded());
+
+        WorldRepository repository = new WorldRepository(trie);
+
+        AccountState result = repository.getAccountState(accountAddress);
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals(accountState.getBalance(), result.getBalance());
+        Assert.assertEquals(accountState.getNonce(), result.getNonce());
     }
 }
