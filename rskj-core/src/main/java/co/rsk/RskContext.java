16c4a59c02383b97042f10c4f3c6cb50db058f70
ajlopez <webmaster@ajlopez.com>
2019-11-02 13:40:00 -0300

    Alternative synchronization in configuration flag

--- rskj-core/src/main/java/co/rsk/RskContext.java
+++ rskj-core/src/main/java/co/rsk/RskContext.java
@@ -956,7 +956,8 @@ public class RskContext implements NodeBootstrapper {
                 rskSystemProperties.getMaxSkeletonChunks(),
                 rskSystemProperties.getChunkSize(),
                 rskSystemProperties.getMaxRequestedBodies(),
-                rskSystemProperties.getLongSyncLimit());
+                rskSystemProperties.getLongSyncLimit(),
+                rskSystemProperties.getAlternativeSync());
     }
 
     protected StateRootHandler buildStateRootHandler() {
@@ -1347,22 +1348,34 @@ public class RskContext implements NodeBootstrapper {
 
     private SyncProcessor getSyncProcessor() {
         if (syncProcessor == null) {
-            syncProcessor = new SyncProcessorImpl(
+            SyncConfiguration syncConfig = this.getSyncConfiguration();
+
+            if (syncConfig.getUseAlternative()) {
+                syncProcessor = new AlternativeSyncProcessorImpl(
                     getBlockchain(),
-                    getBlockStore(),
-                    getConsensusValidationMainchainView(),
-                    getBlockSyncService(),
+                    syncConfiguration,
                     getChannelManager(),
-                    getSyncConfiguration(),
-                    getBlockFactory(),
-                    getProofOfWorkRule(),
-                    new BlockCompositeRule(
-                            new BlockUnclesHashValidationRule(),
-                            new BlockRootValidationRule(getRskSystemProperties().getActivationConfig())
-                    ),
-                    getDifficultyCalculator(),
-                    getPeersInformation(),
-                    getGenesis());
+                    getPeersInformation()
+                );
+            }
+            else {
+                syncProcessor = new SyncProcessorImpl(
+                        getBlockchain(),
+                        getBlockStore(),
+                        getConsensusValidationMainchainView(),
+                        getBlockSyncService(),
+                        getChannelManager(),
+                        getSyncConfiguration(),
+                        getBlockFactory(),
+                        getProofOfWorkRule(),
+                        new BlockCompositeRule(
+                                new BlockUnclesHashValidationRule(),
+                                new BlockRootValidationRule(getRskSystemProperties().getActivationConfig())
+                        ),
+                        getDifficultyCalculator(),
+                        getPeersInformation(),
+                        getGenesis());
+            }
         }
 
         return syncProcessor;
