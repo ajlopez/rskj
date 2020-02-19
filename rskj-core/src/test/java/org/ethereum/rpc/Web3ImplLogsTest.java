7a1e430ff4363cf17d86f87ad25ef69e8aaa89af
ajlopez <webmaster@ajlopez.com>
2020-02-19 18:51:53 -0300

    Data source in BlocksBloomStore

--- rskj-core/src/test/java/org/ethereum/rpc/Web3ImplLogsTest.java
+++ rskj-core/src/test/java/org/ethereum/rpc/Web3ImplLogsTest.java
@@ -1031,7 +1031,7 @@ public class Web3ImplLogsTest {
                 null,
                 new SimpleConfigCapabilities(),
                 null,
-                new BlocksBloomStore(2, 0),
+                new BlocksBloomStore(2, 0, null),
                 mock(Web3InformationRetriever.class));
     }
 
